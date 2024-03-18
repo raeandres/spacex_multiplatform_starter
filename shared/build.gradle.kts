47a72492d001486989581e1073d7a228988147bc
Rae-An Andres <raeandres@users.noreply.github.com>
2024-03-18 21:51:58 +0800

    shared configs
    Added dependencies in shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,7 @@
 
 plugins {
+    kotlin("plugin.serialization").version("1.9.22")
+    id("com.squareup.sqldelight").version("1.5.5")
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
 }
@@ -8,7 +10,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
     }
@@ -23,10 +25,32 @@ kotlin {
             isStatic = true
         }
     }
+
+    val coroutinesVersion = "1.7.3"
+    val ktorVersion = "2.3.7"
+    val sqlDelightVersion = "1.5.5"
+    val dateTimeVersion = "0.4.1"
     
     sourceSets {
+
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+            implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
+        }
+
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
+        }
+
         commonMain.dependencies {
             // put your Multiplatform dependencies here
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
         }
     }
 }
@@ -35,8 +59,8 @@ android {
     namespace = "org.example.project.shared"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
